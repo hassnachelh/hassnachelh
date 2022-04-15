0781b97a756a989682a3449f3bb537e27b0518e2
CH <CH@192.168.56.1>
2022-04-15 11:50:29 +0000

    la méthode add

--- La gestion des etudiants/src/etudiant/listeetudiant.java
+++ La gestion des etudiants/src/etudiant/listeetudiant.java
@@ -3,5 +3,10 @@ package etudiant;
 import java.util.ArrayList;
 
 public class listeetudiant extends ArrayList<etudiant>{
+	@Override
+	public boolean add(etudiant e) {
+		if(this.contains(e)) return false;
+		return super.add(e);
+	}
 
 }
