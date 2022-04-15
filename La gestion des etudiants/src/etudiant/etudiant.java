b804cce1074ed1ad924679ad0e116acf953613c8
CH <CH@192.168.56.1>
2022-04-15 10:59:01 +0000

    Déclaraction les variables Un constructeur

--- La gestion des etudiants/src/etudiant/etudiant.java
+++ La gestion des etudiants/src/etudiant/etudiant.java
@@ -4,5 +4,15 @@ public class etudiant {
 	public static void main(String[]args ) {
 		
 	}
+	private int id;
+	private String nom;
+	private double note;
+	public etudiant(int id,String nom) {
+		this.id=id;
+		this.note=10;
+		this.nom=nom;
+		
+		
+	}
 
 }
